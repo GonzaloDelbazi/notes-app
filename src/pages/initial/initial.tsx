44860aa4527a547f775339d94b9a2edf00072e0f
simonreig <simonreig@gmail.com>
2022-11-03 21:11:17 -0300

    estructuracion de proyecto y primeros estilos

---
+++ src/pages/initial/initial.tsx
@@ -0,0 +1,21 @@
+
+const InitialPage = () => {
+
+  return (
+    <>
+      <div>
+        <h1 className="title-initial-page">Notes App</h1>
+      </div>
+      <div className="initial-boxes">
+        <div className="login-sign-box pointer">
+            <h1>Login</h1>
+                    </div>
+        <div className="login-sign-box pointer">
+            <h1>Sign in</h1>
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default InitialPage;
