29fca75a3d8b3bd38a24b9da09ce515c339bbcf2
gonzalo.delbazi <gonzalo.delbazi@silstech.com.ar>
2022-11-10 15:01:17 -0300

    agrego router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import InitialPage from './pages/initial/initial';
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
 
   return (
-    <div className="App">
-      <InitialPage />
+    <div className="App">\
+    {/* 
+      Router de la App
+      Agregar nuevas rutas: Route
+    */}
+    <Routes>
+      <Route path='/' element={<InitialPage />} />
+    </Routes>
+      
     </div>
   )
 }
