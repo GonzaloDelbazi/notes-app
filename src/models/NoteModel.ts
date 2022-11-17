ef8fa8a1794ccea8e12e9c8d62e7508a9cdb2f19
gonzalo.delbazi <gonzalo.delbazi@silstech.com.ar>
2022-11-16 22:20:00 -0300

    ultimo commit, for de notes, json server, y demas modificaciones

--- src/models/NoteModel.ts
+++ src/models/NoteModel.ts
@@ -1,6 +1,7 @@
 export class NoteModel{
+    id?: number;
     title: string = '';
     description: string = '';
-    isEditable: boolean = false;
-    isEditMode: boolean = false;
+    isEditable?: boolean = false;
+    isEditMode?: boolean = false;
 }
