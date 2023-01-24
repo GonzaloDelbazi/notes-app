5901c150d4fb27f7d0a7a71c75118c866f846828
gonzalo.delbazi <gonzalo.delbazi@silstech.com.ar>
2023-01-24 16:01:57 -0300

    se modifica el initial page para darle otra forma

--- src/pages/initial/initial.tsx
+++ src/pages/initial/initial.tsx
@@ -1,22 +1,26 @@
-import './initial.scss';
+import "./initial.scss";
 
 const InitialPage = () => {
-
   return (
     <>
-      <div className='title-box'>
-        <h1 className="title-initial-page">Notes App</h1>
-      </div>
-      <div className="initial-boxes">
-        <div className="login-box pointer">
-            <h1>Login</h1>
-                    </div>
-        <div className="sign-box pointer">
-            <h1>Sign in</h1>
+      <div className="content">
+        <div className="content-menu">
+          <div className="title-box">
+            <h1 className="title-initial-page">Notes App</h1>
+          </div>
+          <div className="initial-boxes">
+            <div className="login-box pointer">
+              <h1>Login</h1>
+            </div>
+            <div className="sign-box pointer">
+              <h1>Sign in</h1>
+            </div>
+          </div>
         </div>
+        <div className="content-animation"></div>
       </div>
     </>
-  )
+  );
 };
 
 export default InitialPage;
